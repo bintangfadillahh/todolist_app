9be1d6d2a7c5918c8627dccd204ad256951a65e0
star2112 <bintangfadel73@gmail.com>
2024-05-05 07:35:33 +0700

    Separate widget files

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../model/todo.dart';
+
+
+// Widget untuk Item Todo berupa checkbox, title todo, dan delete button
+class TodoItem extends StatelessWidget {
+  final Todo todo;
+  final onTodoChange;
+  final onDeleteItem;
+
+  const TodoItem({
+    super.key,
+    required this.todo,
+    required this.onTodoChange,
+    required this.onDeleteItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          onTodoChange(todo);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+            todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            color: Color(0xFF5F52EE)),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: Container(
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: Color(0xFFCB0F0F),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              onDeleteItem(todo.id);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
